1c94a1f8587441268d9b3c216ed6206c8eef43a6
tapas91 <tapas.g91@gmail.com>
2019-06-24 14:30:02 +0530

    My First Commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AccommodationComponent } from './accommodation/accommodation.component';
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { OffersComponent } from './offers/offers.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+
+const routes: Routes = [
+  { path: 'accommodation', component: AccommodationComponent},
+  { path: 'celebrations', component: CelebrationsComponent},
+  { path: 'food-and-beverage', component: FoodAndBeverageComponent},
+  { path: 'lifestyle', component: LifestyleComponent},
+  { path: 'weddings', component: WeddingsComponent},
+  { path: 'offers', component: OffersComponent},
+  { path: 'signin', component: SigninComponent},
+  { path: 'signup', component: SignupComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
