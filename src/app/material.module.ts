4c0196d8a68eb21ccbdad9e65eea5bfee4d0553c
tapas91 <tapas.g91@gmail.com>
2019-06-27 15:48:09 +0530

    Added Home and Navigation components

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import {MatBadgeModule} from '@angular/material';
-import {MatButtonModule} from '@angular/material';
+import {MatButtonModule,MatListModule,MatTabsModule } from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
-    imports: [MatBadgeModule,MatButtonModule],
-    exports: [MatBadgeModule,MatButtonModule]
+    imports: [MatBadgeModule,MatButtonModule,MatToolbarModule,MatIconModule,MatSidenavModule,MatListModule,MatTabsModule ],
+    exports: [MatBadgeModule,MatButtonModule,MatToolbarModule,MatIconModule,MatSidenavModule,MatListModule,MatTabsModule ]
 })
 
 export class MaterialModule {
