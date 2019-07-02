ad6cbec42f15d922686bf56af3fdbc334acdb82a
tapas91 <tapas.g91@gmail.com>
2019-07-02 15:48:41 +0530

    Updated Home Component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import {MatBadgeModule} from '@angular/material';
-import {MatButtonModule,MatListModule,MatTabsModule } from '@angular/material';
+import {MatButtonModule,MatListModule,MatTabsModule,MatCardModule } from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
-    imports: [MatBadgeModule,MatButtonModule,MatToolbarModule,MatIconModule,MatSidenavModule,MatListModule,MatTabsModule ],
-    exports: [MatBadgeModule,MatButtonModule,MatToolbarModule,MatIconModule,MatSidenavModule,MatListModule,MatTabsModule ]
+    imports: [MatBadgeModule,MatButtonModule,MatToolbarModule,MatIconModule,MatSidenavModule,MatListModule,MatTabsModule,MatCardModule ],
+    exports: [MatBadgeModule,MatButtonModule,MatToolbarModule,MatIconModule,MatSidenavModule,MatListModule,MatTabsModule,MatCardModule ]
 })
 
 export class MaterialModule {
